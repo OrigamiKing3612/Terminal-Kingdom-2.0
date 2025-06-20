8304117a831e161d873e22a08e91d0dedf8335ba
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-06-20 12:47:35 -0500

    Add new item textures

--- scripts/resources/items/tool_item.gd
+++ scripts/resources/items/tool_item.gd
@@ -3,6 +3,7 @@ class_name ToolItem
 
 #@export_group("Tool Settings")
 @export var durability: int = 55
+@export var broken_texture_2d: Texture2D = preload("res://assets/models/icons/bricks_preview.tres")
 
 func remove_durability(count: int = 1):
 	durability -= count
